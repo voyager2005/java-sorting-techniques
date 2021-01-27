3ee2d72392e0877ee453b449b93dd01578f25f29
voyager2005 <76808676+voyager2005@users.noreply.github.com>
2021-01-27 17:07:19 +0530

    Add Files

---
+++ bubbleSort.java
@@ -0,0 +1,54 @@
+package Techsparx.Arrays;
+
+
+/**
+ * Write a description of class bubbleSort here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner; 
+public class bubbleSort
+{
+    public static void main(String args[])
+    {
+        //declaration 
+        Scanner sc = new Scanner(System.in);
+        int n,temp; 
+        int[] a;
+        
+        //reading the number of array elements
+        System.out.println("Enter the number of array elements: ");
+        n = sc.nextInt();
+        
+        //creating an array of size n
+        a = new int[n];
+        
+        //reading the array elements 
+        System.out.println("Enter " + n + " array elements");
+        for(int i = 0 ; i < n ; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        
+        //bubble sort
+        for(int i = 0 ;  i < n-1 ; i++)
+        {
+            for(int j = 0 ; j < n-i-1 ; j++)
+            {
+                if(a[j] > a[j+1])
+                {
+                    temp = a[j] ;
+                    a[j] = a[j+1];
+                    a[j+1] = temp; 
+                }
+            }
+        }
+        
+        //display statement 
+        for(int i = 0 ; i < a.length ; i++)
+        {
+            System.out.println(a[i] + " "); 
+        }
+    }
+}
---
+++ selectionSort.java
@@ -0,0 +1,62 @@
+package Techsparx.Arrays;
+
+
+/**
+ * Write a description of class selectionSort here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner; 
+public class selectionSort
+{
+    public static void main(String args[])
+    {
+        //declaration 
+        Scanner sc = new Scanner(System.in);
+        int n, smallest, indexOfSmallest, temp; 
+        int[] a;
+        
+        //reading the number of array elements
+        System.out.println("Enter the number of array elements: ");
+        n = sc.nextInt();
+        
+        //creating an array of size n
+        a = new int[n];
+        
+        //reading the array elements 
+        System.out.println("Enter " + n + " array elements");
+        for(int i = 0 ; i < n ; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        
+        //selection sort 
+        for(int i = 0 ; i < n-1 ; i++)
+        {
+            //initialization 
+            smallest = a[i] ; 
+            indexOfSmallest = i ; 
+            
+            for(int j = i+1 ; j < n ; j++)
+            {
+                if(a[j] < smallest)
+                {
+                    smallest = a[j]; 
+                    indexOfSmallest = j ; 
+                }
+            }
+            
+            //swpping the array
+            temp = a[i];
+            a[i] = a[indexOfSmallest];
+            a[indexOfSmallest] = temp;
+        }
+        
+        //display statement 
+        for(int i = 0 ; i < a.length ; i++)
+        {
+            System.out.println(a[i] + " "); 
+        }
+    }
+}
